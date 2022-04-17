b73a30a5fca0de4c4729c25f4226280ee1b6a3c5
Lucas Romanelli <xz_lucas@hotmail.com>
2022-04-17 18:28:46 -0300

    Se agrego Modelo Actor y Directos (Many to Many en Movies), se bajo template httml y se adapto main template, index y movie list

--- AppCoder/cineproyecto/models.py
+++ AppCoder/cineproyecto/models.py
@@ -4,16 +4,6 @@ from stat import FILE_ATTRIBUTE_DIRECTORY
 from django.forms import CharField, PasswordInput
 # Create your models here.
 
-class Movies(models.Model):
-    name = models.CharField("Título",max_length=30)
-    dir = models.CharField("Director",max_length=30)
-    act = models.CharField("ActorPrincipal",max_length=30)
-    date = models.DateField("FechaLanzamiento")
-
-    def __str__(self) -> str:
-        return f"{self.name}-{self.dir}" 
-
-
 
 class Users(models.Model):
     name = models.CharField("Nombre",max_length=30)
@@ -31,3 +21,34 @@ class Cinemas(models.Model):
     mail = models.EmailField("Mail")
     phone = models.CharField("Telefono",max_length=20)
     other_info = models.CharField("Otros datos",max_length=30)
+
+class Actors(models.Model):
+    name = models.CharField("Nombre",max_length=30)
+    surname = models.CharField("Apellido",max_length=30)
+    nac = models.CharField("Nacionalidad",max_length=30)
+    birth_date = models.DateField("Nacimiento")
+
+    def __str__(self) -> str:
+        return f"{self.surname}, {self.name}"
+
+class Directors(models.Model):
+    name = models.CharField("Nombre",max_length=30)
+    surname = models.CharField("Apellido",max_length=30)
+    nac = models.CharField("Nacionalidad",max_length=30)
+    birth_date = models.DateField("Nacimiento")
+
+    def __str__(self) -> str:
+        return f"{self.surname}, {self.name}"
+    
+class Movies(models.Model):
+    name = models.CharField("Título",max_length=30)
+    dir = models.CharField("Director",max_length=30)
+    act = models.CharField("ActorPrincipal",max_length=30)
+    date = models.DateField("FechaLanzamiento")
+    actor = models.ManyToManyField(Actors)
+    director = models.ManyToManyField(Directors)
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.dir}" 
+
+
